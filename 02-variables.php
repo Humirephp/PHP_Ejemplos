bb5a0b382bc5c097d25e20b15b98d2cd59a7bb15
Camila-Alva <alvaradocami717@gmail.com>
2024-05-15 17:00:09 -0500

    nuevos ejercicios

---
+++ 02-variables.php
@@ -0,0 +1,14 @@
+<?php
+
+$nombres = "Kevin Celestino";
+$apellidos= "Humire Chalco";
+
+$curso = "BACKEND DEVELOPER WEB";
+$semestre = "tercer semestre";
+
+
+
+echo $nombres." ".$apellidos." ".$curso." ".$semestre;
+
+
+?>
