58dfd37a5c30624f1612f502de9a752d5adbb772
Camila-Alva <alvaradocami717@gmail.com>
2024-05-15 17:35:05 -0500

    Nuevas funciones

--- 05-funciones.php
+++ 05-funciones.php
@@ -7,7 +7,18 @@ function mostrarDatos($nombres,$apellidos,$curso,$semestre){
   ." y estoy en el curso ".$curso." del ".$semestre;
   return $mensaje;
 }
+function calcular($operador,$numero1,$numero2) {
+    if($operador=="+")
+    return $numero1+$numero2;
+    if($operador=="-")
+    return $numero1-$numero2;
+    if($operador=="*")
+    return $numero1*$numero2;
+    if($operador=="/")
+    return $numero1/$numero2;
+}
 echo obtenerSaludo("Jhon")."\n";
 echo mostrarDatos("Jhon","Acero",
-"backend developer web","tercer semestre");
+"backend developer web","tercer semestre")."\n";
+echo "El total de la suma es : ". calcular("+",10,34);
 ?>
