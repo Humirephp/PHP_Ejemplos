3ca488af12754e873836ac105f06d21c39427cc0
norma chalco gonzales <1510720@senati.pe>
2024-05-28 21:13:52 -0500

    Eliminado archivo de registro de pacientes

--- 20-registro-paciente.php
+++ 20-registro-paciente.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF -8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Formulario de Registro de Paciente</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Registro de Paciente</title>
 </head>
 <body>
-    <h1>Resgistro de Paciente</h1>
-    <form id="formulario" action="21-pdo-post.php" method="post">
-        <label for ="nombre">Nombres:</label>
-        <input type ="text" id="nombre" name="nombre" required><br>
-        <label for ="apellido">Apellidos:</label>
+    <h1>Registro de Paciente</h1>
+    <form id="formulario" action="21-pdo-post.php" method="post">    
+        <label for ="nombre">Nombres:</label> 
+        <input type="text" id="nombre" name="nombre" required><br>
+        <label for= "apellido">Apellidos:</label>
         <input type ="text" id="apellido" name="apellido"><br>
-        <label for ="edad">Edad :</label>
-        <input type ="text" id="edad" name="edad" required><br>
+        <label for="edad">Edad :</label>
+        <input type="text" id="edad" name="edad" required /><br>
         <label for="talla">Talla(cm):</label>
-        <input type ="text" id="talla" name="talla" required><br>
+        <input type="text" id="talla" name="talla" required /><br>
         <label for="peso">Peso (kg):</label>
-        <input type ="text" id="peso" name="peso"><br>
+        <input type="text" id="peso" name="peso"><br>
         <h2><u>Sintomas</u></h2>
         <input type="checkbox" name="tos" id="tos">
         <label for="tos">Tos</label><br>
@@ -25,12 +25,12 @@
         <label for="fiebre">Fiebre</label><br>
         <input type="checkbox" name="disnea" id="disnea">
         <label for="disnea">Disnea</label><br>
-        <input type="checkbox" id= dolor_muscular name= "dolor_muscular">
+        <input type="checkbox" id = dolor_muscular name ="dolor_muscular">
         <label for="dolor_muscular">Dolor muscular</label><br>
         <input type="checkbox" id="gripe" name="gripe">
         <label for="gripe">Gripe</label><br>
         <input type="checkbox" id="Presion_alta" name ="Presion_alta">
-        <label for ="Presion_alta">Presion Alta</label><br>
+        <label for="Presion_alta">Presion Alta</label><br>    
         <input type="checkbox" id="Fatiga" name="Fatiga">
         <label for="Fatiga">Fatiga</label><br>
         <input type="checkbox" id="Garraspera" name="Garraspera">
@@ -41,12 +41,10 @@
             <br>
             <button type="submit">Guardar</button>
             <button>Cancelar</button>
-</div>
-</form>
+        </div>
+    </form>    
 </body>
 </html>
-
-
 
 
 
